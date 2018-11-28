aedca025d7ea2962e380fb4292f5dfd889f6054c
Ira Abramov <ira.abramov+github@bluevine.com>
2018-11-28 14:39:20 +0200

    don't fire up all the bashit plugins. turn a few off.

--- install_bash_it.sh
+++ install_bash_it.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "*** Installing bash-it with some sensible defaults"
+
 export BASH_IT="$HOME/.bash_it"
 
 if [ ! -d "$BASH_IT" ]; then
@@ -9,16 +11,24 @@ fi
 source "$BASH_IT"/bash_it.sh
 
 # Completions
-bash-it enable completion makefile pip rake virtualbox ssh system tmux \
-  vagrant git git_flow gem docker docker-compose dirs defaults bash-it \
-  hub homesick awscli export fabric pip3 terraform test_kitchen vault
+bash-it enable completion makefile pip ssh system \
+  vagrant git gem docker defaults bash-it \
+  hub homesick export fabric pip3
 
 # Plugins
-bash-it enable plugin alias-completion base battery browser docker \
-  docker-compose dirs edit-mode-emacs gif git history less-pretty-cat \
-  python ruby ssh tmux virtualenv z_autoenv fzf aws docker-machine \
-  fasd hub osx osx-timemachine powerline visual-studio-code
+bash-it enable plugin alias-completion base browser \
+  edit-mode-emacs git history fasd visual-studio-code \
+  python ruby ssh virtualenv
 
 # Aliases
-bash-it enable alias ag apt docker emacs vim atom git general fuck tmux \
-    vagrant homesick curl homebrew homebrew-cask osx vault
+bash-it enable alias ag apt vim atom git general \
+    vagrant homesick curl homebrew homebrew-cask
+
+echo "*** More options I can recommend if relevant for you:"
+echo "bash-it enable completion tmux git_flow docker-compose dirs awscli \\\
+      test_kitchen terraform"
+echo
+echo "bash-it enable plugin dirs battery docker docker-compose gif less-pretty-cat \\\
+      tmux z_autoenv hub fzf aws docker-machine osx-timemachine powerline"
+echo
+echo "bash-it enable alias docker emacs atom fuck tmux osx"
