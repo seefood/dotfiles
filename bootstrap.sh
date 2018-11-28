dc32abc4eaed3a7273bbfddc9393d8e467742005
Ira Abramov <ira.abramov+github@bluevine.com>
2018-11-28 10:22:52 +0200

    unify Linux and mac as much as possible.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,10 @@ set -e
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  hash brew 2>/dev/null || (echo "install brew first" && exit 1)
+  hash brew 2>/dev/null || \
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   brew doctor
-  #brew tap homebrew/versions
   brew tap caskroom/versions
   brew tap caskroom/fonts
   brew tap chef/chef
@@ -16,7 +15,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "getting some important extra brew packages"
   brew install homesick-completion screen neovim the_silver_searcher git \
     curl wget bash hub fzf fd
-  pip3 install thefuck neovim powerline-status
 
   cd ~/Downloads/
   wget -c https://www.python.org/ftp/python/3.6.4/python-3.6.4-macosx10.6.pkg \
@@ -26,37 +24,22 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
   python3 --version | grep "3\.6" || (echo "Python version is not 3.6.X, please install https://www.python.org/ftp/python/3.6.4/python-3.6.4-macosx10.6.pkg" && exit 1)
 
-  echo "installing homesick"
-
-  sudo gem install homesick --no-ri --no-rdoc
-
-  # Have ensured that homesick is available
-  hash homesick 2>/dev/null || (echo "homesick install failed" && exit 1)
-
 elif [[ "$(lsb_release -is)" == "Ubuntu" ]] || [[ "$(lsb_release -is)" == "Debian" ]] ; then
-  echo "Doing Debian install of homesick"
+
+  hash brew 2>/dev/null || \
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+  brew doctor
+  brew tap caskroom/versions
+  brew tap caskroom/fonts
+  brew tap chef/chef
 
   echo "installing some essential packages"
   sudo apt-get install -y screen silversearcher-ag curl thefuck git \
-      software-properties-common python3-pip
-  pip3 install --upgrade powerline-status
-  pip3 install --upgrade neovim
+      software-properties-common python3-pip ruby
 
   # Adding backports, neovim and other useful bits.
   sudo add-apt-repository -u "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -cs)-backports main restricted universe multiverse"
-
-  if hash homesick 2>/dev/null; then
-    echo "homesick appears to be installed"
-  else
-    echo "installing ruby gems"
-    sudo apt-get install -y ruby
-
-    echo "installing homesick"
-
-    sudo gem install homesick --no-ri --no-rdoc
-  fi
-  # Have ensured that homesick is available
-  hash homesick 2>/dev/null || (echo "homesick install failed" && exit 1)
 
   if ! hash nvim ; then
     echo "install neovim, trying from default sources"
@@ -77,39 +60,20 @@ elif [[ "$(lsb_release -is)" == "Ubuntu" ]] || [[ "$(lsb_release -is)" == "Debia
 
   mkdir -p ~/bin
 
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+  brew install fzf fd hub
 
-  # Download Hub
-  if [ ! -e "$HOME/bin/hub" ] && [ ! -e /usr/local/bin/hub ] ; then
-    HUB_VERSION=2.6.0
-    HUB_DOWNLOAD_URL=https://github.com/github/hub/releases/download
-    HUB_OS=hub-linux-amd64
-    FULL_URL=${HUB_DOWNLOAD_URL}/v${HUB_VERSION}/${HUB_OS}-${HUB_VERSION}.tgz
-
-    echo "Downloading Hub ${HUB_VERSION}"
-
-    curl -fL ${FULL_URL} > /tmp/hub.tgz && \
-      tar zxf /tmp/hub.tgz -C /tmp && \
-      mv /tmp/${HUB_OS}-${HUB_VERSION}/bin/hub ~/bin/hub
-
-    rm -Rf /tmp/hub*
-  else
-    echo "Hub exists."
-  fi
-
-  # Download fd
-  if hash fd; then
-    echo "fd exists."
-  else
-    fdversion=7.2.0
-    if [[ "$(lsb_release -is)" == "Ubuntu" ]] || [[ "$(lsb_release -is)" == "Debian" ]] ; then
-      wget https://github.com/sharkdp/fd/releases/download/v${fdversion}/fd_${fdversion}_amd64.deb && \
-        sudo dpkg -i fd_${fdversion}_amd64.deb
-    fi
-
-    rm -Rf fd_${fdversion}_amd64.deb
-  fi
 fi
+
+echo "installing homesick"
+if hash homesick 2>/dev/null; then
+  echo "homesick appears to be installed"
+else
+  sudo gem install homesick --no-ri --no-rdoc
+fi
+# Have ensured that homesick is available
+hash homesick 2>/dev/null || (echo "homesick install failed" && exit 1)
+
+pip3 install --upgrade powerline-status neovim thefuck
 
 ## Clone dotfiles
 homesick clone bluevine-dev/dotfiles dotfiles
