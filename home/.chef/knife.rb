39c8b6ce71be3e2ec0ebb5759a33c8e78d04b65c
Ira Abramov <ira.abramov+github@bluevine.com>
2018-12-04 09:57:37 +0200

    Removing duplicate variable name

--- home/.chef/knife.rb
+++ home/.chef/knife.rb
@@ -6,13 +6,13 @@ log_location           STDOUT
 node_name              `whoami`.strip
 # The file specified by "client_key" must contain the private key generated for
 # your user on the chef server.
-client_key             "#{c_folder}/#{ENV['CHEF_ENVIRONMENT']}/user.pem"
+client_key             "#{c_folder}/#{ENV['FABRIC_AWS_ENVIRONMENT']}/user.pem"
 validation_client_name 'chef-validator'
-validation_key         "#{c_folder}/#{ENV['CHEF_ENVIRONMENT']}/validator.pem"
+validation_key         "#{c_folder}/#{ENV['FABRIC_AWS_ENVIRONMENT']}/validator.pem"
 cache_type             'BasicFile'
 cookbook_path          ["#{ENV['CHEF_REPOSITORY']}/cookbooks"]
 cache_options(:path => "#{c_folder}/checksums")
-case ENV['CHEF_ENVIRONMENT']
+case ENV['FABRIC_AWS_ENVIRONMENT']
     when "staging"
         chef_server_url "https://staging-chef-server.bluevine.com/organizations/bluevine/"
     when "production"
