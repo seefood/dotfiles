8e4b20eb594c635f29da5121764efd9b6e10eb69
Ira Abramov <ira.abramov+github@bluevine.com>
2018-12-09 19:53:07 +0200

    Looks like this is already taken care of by brew.

--- install_dev.sh
+++ install_dev.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" == "darwin"* ]] ; then
 
   brew cask install diffmerge
   rm /usr/local/bin/diffmerge
-  ln -s /Applications/DiffMerge.app/Contents/Resources/diffmerge.sh /usr/local/bin/diffmerge
+  #ln -s /Applications/DiffMerge.app/Contents/Resources/diffmerge.sh /usr/local/bin/diffmerge
 
   brew cask install virtualbox vagrant vagrant-manager
 else
