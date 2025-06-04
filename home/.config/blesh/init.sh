d7f501a3ca1b7ff9eaf3ad71ba012dc32720de0e
Ira Abramov <ira@agrematch.com>
2025-06-04 11:57:47 +0300

    small tweaks to the prompt theme, blesh config.

---
+++ home/.config/blesh/init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# blerc (Example)
+
+bleopt char_width_mode=east
+bleopt input_encoding=UTF-8
+bleopt edit_bell=vbell vbell_{default_message=' BEL ',duration=3000,align=right}
+bleopt history_default_point=end
---
+++ home/.config/fish/completions/mc.fish
@@ -0,0 +1,8 @@
+
+function __complete_mc
+    set -lx COMP_LINE (commandline -cp)
+    test -z (commandline -ct)
+    and set COMP_LINE "$COMP_LINE "
+    /opt/homebrew/bin/mc
+end
+complete -f -c mc -a "(__complete_mc)"
